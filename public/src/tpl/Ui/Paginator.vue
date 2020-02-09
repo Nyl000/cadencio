980eb0bce424d7f73452f17b7c7ced639ee483ad
Loïc <loic@webismymind.be>
2020-02-09 01:38:32 +0100

    fixed paginator arrow

--- public/src/tpl/Ui/Paginator.vue
+++ public/src/tpl/Ui/Paginator.vue
@@ -23,7 +23,7 @@
 <script>
 
     import ChevronLeftIcon from 'vue-material-design-icons/ChevronLeft.vue';
-    import ChevronRightIcon from 'vue-material-design-icons/ChevronLeft.vue';
+    import ChevronRightIcon from 'vue-material-design-icons/ChevronRight.vue';
 
 
     export default {
