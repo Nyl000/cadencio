0d5837509c20d123abd18f9c6dfd16beafc5210d
Loïc <loic@webismymind.be>
2020-04-06 00:47:44 +0200

    settings permission

--- src/Config/Permissions.php
+++ src/Config/Permissions.php
@@ -30,6 +30,9 @@ class Permissions {
             ],
             'notifications' => [
                 '*',
+            ],
+            'settings' => [
+                '*'
             ]
         ];
 
