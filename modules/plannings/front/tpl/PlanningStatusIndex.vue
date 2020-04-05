3f86607475370cb0b3f12625f77b2a518426b4d7
Loïc <loic@webismymind.be>
2020-04-05 17:38:05 +0200

    fix syntax error

--- modules/plannings/front/tpl/PlanningStatusIndex.vue
+++ modules/plannings/front/tpl/PlanningStatusIndex.vue
@@ -2,111 +2,124 @@
     <div class="planning_status_index">
         <div class="tablecontainer">
             <div class="actionbar">
-                <button class="button button-add" v-on:click="addPlanningStatusModal"><plus-icon /> Add</button>
-                <button class="button" v-on:click="refreshGrid()"><sync-icon/></button>
+                <button class="button button-add" v-on:click="addPlanningStatusModal">
+                    <plus-icon/>
+                    Add
+                </button>
+                <button class="button" v-on:click="refreshGrid()">
+                    <sync-icon/>
+                </button>
             </div>
 
-            <entity-table ref="table" :model="planningStatusModel" :definition="tableDefinition" :page="this.$route.params.page || 1" />
+            <entity-table ref="table" :model="planningStatusModel" :definition="tableDefinition"
+                          :page="this.$route.params.page || 1"/>
 
         </div>
         <Modale ref="addPlanningStatusModal">
-            <PlanningStatusAdd v-bind:onAdded="onPlanningStatusAdded" />
+            <PlanningStatusAdd v-bind:onAdded="onPlanningStatusAdded"/>
         </Modale>
     </div>
 </template>
 
 <script>
 
-    import {hasPermission} from 'js/Models/User';
-    import {deleteItem}  from 'js/Models/PlanningStatus';
-    import Modale from 'tpl/Ui/Modale.vue';
-    import PlanningStatusAdd from 'tpl/PlanningStatusAdd.vue';
-    import PlusIcon from 'vue-material-design-icons/Plus.vue';
-    import SyncIcon from 'vue-material-design-icons/Sync.vue';
-    import EntityTable from 'tpl/Ui/EntityTable';
-    import DeleteIcon from 'vue-material-design-icons/Delete.vue';
-    const planningStatusModel = require('js/Models/PlanningStatus');
-    import EditableText from 'tpl/Ui/EditableText.vue';
-    import EditableColor from 'tpl/Ui/EditableColor.vue';
-    import EditableCheckbox from 'tpl/Ui/EditableCheckbox.vue';
+	import {hasPermission} from 'js/Models/User';
+	import {deleteItem}  from 'js/Models/PlanningStatus';
+	import Modale from 'tpl/Ui/Modale.vue';
+	import PlanningStatusAdd from 'tpl/PlanningStatusAdd.vue';
+	import PlusIcon from 'vue-material-design-icons/Plus.vue';
+	import SyncIcon from 'vue-material-design-icons/Sync.vue';
+	import EntityTable from 'tpl/Ui/EntityTable';
+	import DeleteIcon from 'vue-material-design-icons/Delete.vue';
+	const planningStatusModel = require('js/Models/PlanningStatus');
+	import EditableText from 'tpl/Ui/EditableText.vue';
+	import EditableColor from 'tpl/Ui/EditableColor.vue';
+	import EditableCheckbox from 'tpl/Ui/EditableCheckbox.vue';
 
 
-    export default {
-        data: function() {
-            return {
-                planningStatusModel:planningStatusModel,
-                tableDefinition : {},
-            }
-        },
-        mounted: function () {
+	export default {
+		data: function () {
+			return {
+				planningStatusModel: planningStatusModel,
+				tableDefinition: {},
+			}
+		},
+		mounted: function () {
 
-            this.refreshGrid();
-            this.refreshTableDatas();
+			this.refreshGrid();
+			this.refreshTableDatas();
 
-        },
-        methods: {
+		},
+		methods: {
 
-        	refreshTableDatas : function() {
-        		this.tableDefinition = {
+			refreshTableDatas: function () {
+				this.tableDefinition = {
 					idField: 'id',
-					saveurl:'/planning_status/{id}',
-					columns : [
-						{property: 'title', label : 'Title', sortable : true, renderer : {
-							type : EditableText,
+					saveurl: '/planning_status/{id}',
+					columns: [
+						{
+							property: 'title', label: 'Title', sortable: true, renderer: {
+							type: EditableText,
 							placeholder: 'Title',
-							canUpdate : hasPermission('planning_status','update'),
-						}},
-						{property: 'color', label : 'Color', sortable : false, renderer : {
-							type : EditableColor,
+							canUpdate: hasPermission('planning_status', 'update'),
+						}
+						},
+						{
+							property: 'color', label: 'Color', sortable: false, renderer: {
+							type: EditableColor,
 							placeholder: 'Color',
-							canUpdate : hasPermission('planning_status','update'),
-						}},
-						{property: 'closed', label : 'Closed', sortable : true, renderer : {
-							type : EditableCheckbox,
+							canUpdate: hasPermission('planning_status', 'update'),
+						}
+						},
+						{
+							property: 'closed', label: 'Closed', sortable: true, renderer: {
+							type: EditableCheckbox,
 							placeholder: 'Closed',
-							canUpdate : hasPermission('planning_status','update'),
-						}},
+							canUpdate: hasPermission('planning_status', 'update'),
+						}
+						},
 					],
-					actions : [
-						{ action : this.deleteItem, component : DeleteIcon, canDisplay : hasPermission('planning_status','delete')  },
+					actions: [
+						{
+							action: this.deleteItem,
+							component: DeleteIcon,
+							canDisplay: hasPermission('planning_status', 'delete')
+						},
 					]
-                };
-            },
-            hasPermission: (resource, action) => {
-                return hasPermission(resource, action);
-            },
-            deleteItem: function (userId) {
-                if (confirm('Confirmez que vous voulez supprimer le status')) {
-                    deleteItem(userId).then(() => {
-                        this.refreshGrid();
-                    })
-                }
-            },
-            refreshGrid: function () {
-                this.$refs.table.refresh();
-            },
+				};
+			},
+			hasPermission: (resource, action) => {
+				return hasPermission(resource, action);
+			},
+			deleteItem: function (userId) {
+				if (confirm('Confirmez que vous voulez supprimer le status')) {
+					deleteItem(userId).then(() => {
+						this.refreshGrid();
+					})
+				}
+			},
+			refreshGrid: function () {
+				this.$refs.table.refresh();
+			},
 
-            addPlanningStatusModal: function () {
-                this.$refs.addPlanningStatusModal.show();
-            },
-            onPlanningStatusAdded: function () {
-                this.$refs.addPlanningStatusModal.hide();
-                this.refreshGrid();
-            }
+			addPlanningStatusModal: function () {
+				this.$refs.addPlanningStatusModal.show();
+			},
+			onPlanningStatusAdded: function () {
+				this.$refs.addPlanningStatusModal.hide();
+				this.refreshGrid();
+			}
 
-        },
+		},
 
 
-    }
-    },
-
-    components: {
-        Modale,
-        PlanningStatusAdd,
-        PlusIcon,
-        SyncIcon,
-        EntityTable
-        }
-    }
+		components: {
+			Modale,
+			PlanningStatusAdd,
+			PlusIcon,
+			SyncIcon,
+			EntityTable
+		}
+	}
 
 </script>
