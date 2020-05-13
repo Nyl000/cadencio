70c64d21f926a321c43e6e8e93db58c04ba10072
Loïc <loic@webismymind.be>
2020-05-14 00:37:20 +0200

    Setting model -> handle global app settings storage

---
+++ src/Cadencio/Models/SettingModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Cadencio\Models;
+
+use Cadencio\Application;
+use Cadencio\Services\ModulesManager;
+
+class SettingModel extends AbstractModel
+{
+
+    protected $modelName = 'settings';
+    protected $resourceName = 'settings';
+    protected $identifier = 'name';
+
+
+    public function getSearchField()
+    {
+        return ['name'];
+    }
+
+    public function patch($id, $datas, $uniqueFieldname = 'id') {
+        if (isset($datas->active) && $datas->active == 1) {
+
+            $current = $this->getOne($id,$uniqueFieldname);
+            if($current['active'] == 0) {
+                ModulesManager::getInstance()->registerModule($current['name']);
+                $instance = Application::$instance->getModuleInstance($current['name']);
+                $instance->onActivate();
+            }
+
+        }
+        return parent::patch($id,$datas,'name');
+    }
+
+    public function idExists($id, $field = 'id')
+    {
+        return parent::idExists($id,'name');
+    }
+
+    public function getOne($id, $field = 'id',$ignoreCase = false) {
+        return parent::getOne($id, 'name',$ignoreCase = false) ;
+
+    }
+
+
+}
