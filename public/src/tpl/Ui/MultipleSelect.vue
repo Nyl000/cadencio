1e07cc558d1486d146d00131971f625fdfa0bb2b
Loïc <loic@webismymind.be>
2020-05-19 00:33:21 +0200

    MultipleSelect component

---
+++ public/src/tpl/Ui/MultipleSelect.vue
@@ -0,0 +1,70 @@
+<template>
+
+    <div>
+        <div>
+            <div class="title">{{title}}</div>
+            <div class="groupitems">
+                <div>
+                    <span class="item" :key="item[identifier]" v-for="item in selectedItems">{{item[label]}}</span>
+                    <em class="empty" v-if="Object.keys(selectedItems).length == 0">No entry</em>
+                </div>
+                <div class="button primary " @click.prevent="openSearch">Search</div>
+            </div>
+        </div>
+        <Modale ref="searchModale">
+            <MultipleSearch
+                    :model="model"
+                    :id_excluded="id_excluded"
+                    :onSelected="onChange"
+                    :selectedItems="selectedItems"
+                    :placeholder="search_placeholder"
+                    :identifier="identifier"
+                    :label="label"
+            />
+        </Modale>
+    </div>
+
+</template>
+
+
+<script>
+
+	import MultipleSearch from 'tpl/Ui/MultipleSearch';
+	import Modale from 'tpl/Ui/Modale';
+
+	export default {
+		props: [
+			'selectedItems',
+            'id_excluded',
+            'model',
+            'search_placeholder',
+            'identifier',
+            'label',
+            'title',
+            'onChangeCallback',
+        ],
+
+		mounted: async function () {
+
+
+		},
+		methods: {
+			openSearch: function () {
+				this.$refs.searchModale.show();
+			},
+			onChange: function (items) {
+				this.$forceUpdate();
+				if(typeof this.onChangeCallback === 'function') {
+					this.onChangeCallback(items)
+                }
+			}
+
+		},
+
+
+		components: {
+			Modale,
+			MultipleSearch
+		}
+	}
+</script>
