5c36ed597946585acc5b15b37149becfb199fce5
Loïc <loic@webismymind.be>
2020-05-19 15:31:56 +0200

    Loader component

---
+++ public/src/tpl/Ui/Loader.vue
@@ -0,0 +1,18 @@
+<template>
+    <transition>
+        <div class="loader">
+            <div class="spin">
+                <loading-icon/>
+            </div>
+        </div>
+    </transition>
+
+</template>
+
+<script>
+	import LoadingIcon from 'vue-material-design-icons/Loading.vue';
+	export default {
+		components: {LoadingIcon}
+	}
+</script>
+
