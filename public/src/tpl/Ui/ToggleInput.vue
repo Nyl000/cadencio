afdc7d06a19b686a6d2c5f168ecf81c4224f47e6
Loïc <loic@webismymind.be>
2020-05-28 00:45:30 +0200

    Toggle component

---
+++ public/src/tpl/Ui/ToggleInput.vue
@@ -0,0 +1,18 @@
+<template>
+   <div @click="toggle" :class="'toggle ' + (value ? 'on':'off')">
+       <span class="slider"></span>
+   </div>
+</template>
+
+
+<script>
+	export default {
+		props: ['value'],
+        methods : {
+            toggle : function() {
+            	this.$emit('input', !this.value);
+            }
+        }
+
+	}
+</script>
