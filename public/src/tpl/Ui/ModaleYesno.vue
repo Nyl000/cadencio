35fa5f3a5d2b48ba341c0ef1d8a77c341d77d159
Loïc <loic@webismymind.be>
2020-06-11 15:36:00 +0200

    yes/no modale

---
+++ public/src/tpl/Ui/ModaleYesno.vue
@@ -0,0 +1,38 @@
+<template>
+    <Modale ref="modale" class="yesno_modale">
+        <div class="message">
+            {{message}}
+        </div>
+        <div class="buttons">
+            <button class="button button-no" @click.prevent="choose(0)">No</button>
+            <button class="button button-yes" @click.prevent="choose(1)">Yes</button>
+        </div>
+
+    </Modale>
+</template>
+
+<script>
+    import Modale from 'tpl/Ui/Modale.vue';
+
+    export default {
+        props: ['message'],
+        data: () => {
+            return {
+                visible: false,
+            }
+        },
+        methods: {
+
+            trigger() {
+                this.$refs.modale.show();
+            },
+            choose(state, triggerHide) {
+                if (typeof triggerHide === 'undefined') triggerHide = true;
+                this.$emit('choose', state);
+                if (triggerHide) this.$refs.modale.hide();
+            }
+
+        },
+        components: {Modale}
+    }
+</script>
