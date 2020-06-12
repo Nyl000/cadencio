af9ead4cefdf8e03ab0a20931cde3a44abfa96ea
Loïc <loic@webismymind.be>
2020-06-12 14:45:51 +0200

    Assign a default timezone to a new user

--- src/Cadencio/Models/UserModel.php
+++ src/Cadencio/Models/UserModel.php
@@ -19,48 +19,51 @@ class UserModel extends AbstractModel
         }
     }
 
-    public function isAdministrator($idUser) {
+    public function isAdministrator($idUser)
+    {
 
         return
             $this->getAdapter()->fetchOne(
                 'SELECT COUNT(*) 
                         FROM roles_resources 
                         JOIN users ON users.id_role = roles_resources.id_role
-                        WHERE users.id = ? AND resource_name = ? AND resource_right = ?', [$idUser,'*','*']
+                        WHERE users.id = ? AND resource_name = ? AND resource_right = ?', [$idUser, '*', '*']
             ) > 0;
 
     }
 
     public function getPublicFields()
     {
-        return ['id','email','id_role','name','firstname','nickname'];
+        return ['id', 'email', 'id_role', 'name', 'firstname', 'nickname'];
     }
 
-    public function getSearchField() {
-        return ['id','email'];
+    public function getSearchField()
+    {
+        return ['id', 'email'];
     }
 
-    public function login($email,$password) {
-        $userId = $this->getAdapter()->fetchOne('SELECT id FROM '.$this->modelName. ' WHERE email = ? AND password = ?', [$email, hash('sha256',$password)]);
+    public function login($email, $password)
+    {
+        $userId = $this->getAdapter()->fetchOne('SELECT id FROM ' . $this->modelName . ' WHERE email = ? AND password = ?', [$email, hash('sha256', $password)]);
         return $userId;
     }
 
-    public function getHashedPassword($userId) {
-        return $this->getAdapter()->fetchOne('SELECT password FROM '.$this->modelName.' WHERE id = ?', [$userId]);
+    public function getHashedPassword($userId)
+    {
+        return $this->getAdapter()->fetchOne('SELECT password FROM ' . $this->modelName . ' WHERE id = ?', [$userId]);
     }
 
     public function createOrUpdate($datas)
     {
         if (is_object($datas)) {
-            $datas = (array) $datas;
+            $datas = (array)$datas;
         }
 
         if (!isset($datas['id']) || empty($datas['id'])) {
             unset($datas['id']);
             $datas['date_register'] = date('Y-m-d H:i:s');
             $datas['hash'] = hash('SHA256', uniqid());
-        }
-        else {
+        } else {
             if (isset($datas['id_role'])) {
                 $roleModel = new RoleModel();
                 if (!$this->isAdministrator(Application::$instance->getCurrentUserId() && $roleModel->isAdministrator($datas['id_role']))) {
@@ -68,13 +71,23 @@ class UserModel extends AbstractModel
                 }
             }
         }
-        return parent::createOrUpdate($datas);
+        $id = parent::createOrUpdate($datas);
+        if (!isset($datas['id'])) {
+            //create default user options
+            $modelOptions = new UserOptionModel();
+            $modelOptions->createOrUpdate([
+                'id_user' => $id,
+                'key' => 'timezone',
+                'value' => 'Europe/Brussels',
+            ]);
+        }
+        return $id;
     }
 
     public function patch($id, $datas, $uniqueFieldname = 'id')
     {
         if (is_object($datas)) {
-            $datas = (array) $datas;
+            $datas = (array)$datas;
         }
 
         if (isset($datas['id_role'])) {
@@ -94,12 +107,13 @@ class UserModel extends AbstractModel
         return parent::delete($id);
     }
 
-    public function getOne($id, $field = 'id',$ignoreCase = true) {
+    public function getOne($id, $field = 'id', $ignoreCase = true)
+    {
 
         $roleModel = new RoleModel();
         $optionsModel = new UserOptionModel();
 
-        $user = parent::getOne($id,$field,$ignoreCase);
+        $user = parent::getOne($id, $field, $ignoreCase);
 
         $user['role'] = $roleModel->getOne($user['id_role']);
         $user['options'] = $optionsModel->getByUser(($user['id']));
