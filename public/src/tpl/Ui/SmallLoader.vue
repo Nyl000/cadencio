e08ce2c8b2818f52efccec1134b35c730fc689e9
Loïc <loic@webismymind.be>
2020-06-13 21:20:33 +0200

    Added a loader to the login form

---
+++ public/src/tpl/Ui/SmallLoader.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="spin spin-inline">
+        <loading-icon/>
+    </div>
+</template>
+
+<script>
+    import LoadingIcon from 'vue-material-design-icons/Loading.vue';
+    export default {
+        components: {LoadingIcon}
+    }
+</script>
