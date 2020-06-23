14f875a0b52f06e2959297c3f67d965672e399d9
Loïc <loic@webismymind.be>
2020-06-23 15:26:25 +0200

    New Toast UI element + solved some z-index issues

---
+++ public/src/tpl/Ui/Toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <transition name="fadedown">
+        <div :class="'toast '+state" v-if="visible">
+            {{message}}
+        </div>
+    </transition>
+
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                message: '',
+                visible: false,
+                state : '',
+            }
+        },
+        methods: {
+            show(message, state, delaySeconds) {
+                this.message = message;
+                this.visible = true;
+                this.state = state;
+                setTimeout(() => {
+                    this.visible = false
+                }, delaySeconds * 1000);
+            }
+        }
+    }
+</script>
+
