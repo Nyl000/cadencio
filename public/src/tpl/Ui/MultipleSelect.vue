afc976763926b52a42be4977d527240a08404c34
Loïc <loic@webismymind.be>
2021-04-08 11:50:32 +0200

    Added delete icons to multiplesearch selections

--- public/src/tpl/Ui/MultipleSelect.vue
+++ public/src/tpl/Ui/MultipleSelect.vue
@@ -8,7 +8,10 @@
             </div>
             <div class="groupitems">
                 <div>
-                    <span class="item" :key="item[identifier]" v-for="item in selectedItems">{{item[label]}}</span>
+                    <div class="item" :key="item[identifier]" v-for="item in selectedItems">
+                        {{item[label]}}
+                        <span class="close" @click="deleteItem(item[identifier])"><close-icon title="remove" /></span>
+                    </div>
                     <em class="empty" v-if="Object.keys(selectedItems).length == 0">{{empty_label || 'No entry' }}</em>
                 </div>
                 <a href="#"  @click.prevent="openSearch">{{modify_button_label || 'Modify'}}</a>
@@ -36,6 +39,7 @@
 
 	import MultipleSearch from 'tpl/Ui/MultipleSearch';
 	import Modale from 'tpl/Ui/Modale';
+    import CloseIcon from 'vue-material-design-icons/Close.vue';
 
 	export default {
 		props: [
@@ -68,14 +72,23 @@
 				if(typeof this.onChangeCallback === 'function') {
 					this.onChangeCallback(items)
                 }
-			}
+			},
+
+            deleteItem  : function(identifier) {
+			  let items = this.selectedItems;
+			  delete items[identifier];
+                if(typeof this.onChangeCallback === 'function') {
+                    this.onChangeCallback(items)
+                }
+            }
 
 		},
 
 
 		components: {
 			Modale,
-			MultipleSearch
+			MultipleSearch,
+            CloseIcon
 		}
 	}
 </script>
