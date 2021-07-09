3b282d390f4db10d3769ea77a0c59ade8f61b2d8
Loïc <loic@webismymind.be>
2021-07-09 16:44:23 +0200

    Install Vuex

--- public/src/app.js
+++ public/src/app.js
@@ -2,14 +2,16 @@ import "vue-swatches/dist/vue-swatches.min.css"
 import 'vue-simple-markdown/dist/vue-simple-markdown.css';
 import 'vue-material-design-icons/styles.css';
 import 'vue-datetime/dist/vue-datetime.min.css'
-
 import 'css/app.scss'
 import VueSimpleMarkdown from 'vue-simple-markdown';
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
 
 import {Main} from 'js/App';
 
+Vue.use(Vuex)
 Vue.use(VueRouter);
 Vue.use(VueSimpleMarkdown);
 
