ce3612719245797f1bb43a81d68848f2bca171a8
Loïc <loic@webismymind.be>
2021-07-30 01:03:21 +0200

    Settings material design

--- public/src/tpl/SettingsIndex.vue
+++ public/src/tpl/SettingsIndex.vue
@@ -1,116 +1,111 @@
 <template>
-    <div class="settingsindex">
-        <div class="columns">
-            <div class="medium6">
-                <div class="roundedbox">
-                    <div class="topbar">
-                        <h2>{{$t('Email Settings')}}</h2>
-                    </div>
-                    <div class="inner">
-                        <h3>SMTP</h3>
-                        <div class="formInput">
-                            <label>{{$t('Server')}}</label>
-                            <input type="text" v-model="mail_smtp_host"/>
-                        </div>
-                        <div class="formInput">
-                            <label>{{$t('Port')}}</label>
-                            <input type="text" v-model="mail_smtp_port"/>
-                        </div>
-                        <div class="formInput">
-                            <input type="password" v-model="password" v-show="false"/>
-                            <label>{{$t('Username')}}</label>
-                            <input type="text" v-model="mail_smtp_user"/>
-                        </div>
-                        <div class="formInput">
-                            <label>{{$t('Password')}}</label>
-                            <input type="password" v-model="mail_smtp_password" autocomplete="new-password"/>
-                        </div>
-                        <div class="formInput">
-                            <label>{{$t('From Email')}}</label>
-                            <input type="text" v-model="mail_smtp_frommail"/>
-                        </div>
-                        <div class="formInput">
-                            <label>{{$t('From Name')}}</label>
-                            <input type="text" v-model="mail_smtp_fromname"/>
-                        </div>
-                        <button class="button success" v-on:click="saveEmail">
-                            {{$t('Save')}}
-                        </button>
-                        <button class="button" v-on:click="sendTestEmail">
-                            {{$t('Send me a test email')}}
-                        </button>
-                    </div>
-                </div>
-            </div>
+    <div class="settingsindex md-layout md-alignment-top-left">
+        <div class="md-layout-item md-size-33 md-large-size-33 md-small-size-80 md-xsmall-size-100">
+
+            <md-card>
+                <md-card-header>
+                    <h2>{{$t('Email Settings')}}</h2>
+                </md-card-header>
+                <md-card-content>
+                    <h3>{{$t('SMTP')}}</h3>
+                    <md-field>
+                        <label for="mail_smtp_port">{{$t('Port')}}</label>
+                        <md-input name="mail_smtp_port" id="mail_smtp_port" autocomplete="mail_smtp_port" v-model="mail_smtp_port"/>
+                    </md-field>
+                    <md-field>
+                        <label for="mail_smtp_user">{{$t('Username')}}</label>
+                        <md-input  name="mail_smtp_user" id="mail_smtp_user" autocomplete="mail_smtp_user" v-model="mail_smtp_user"/>
+                    </md-field>
+                    <md-field>
+                        <label for="mail_smtp_password">{{$t('Password')}}</label>
+                        <md-input type="password" name="mail_smtp_password" id="mail_smtp_password" autocomplete="mail_smtp_password" v-model="mail_smtp_password"/>
+                    </md-field>
+                    <h3>{{$t('Identity')}}</h3>
+                    <md-field>
+                        <label for="mail_smtp_frommail">{{$t('From Email')}}</label>
+                        <md-input type="email"  name="mail_smtp_frommail" id="mail_smtp_frommail" autocomplete="mail_smtp_password" v-model="mail_smtp_frommail"/>
+                    </md-field>
+                    <md-field>
+                        <label for="mail_smtp_frommail">{{$t('From Name')}}</label>
+                        <md-input  name="mail_smtp_fromname" id="mail_smtp_fromname" autocomplete="mail_smtp_fromname" v-model="mail_smtp_fromname"/>
+                    </md-field>
+                </md-card-content>
+                <md-card-actions>
+                    <md-button class="md-primary md-raised" v-on:click="saveEmail">
+                        {{$t('Save')}}
+                    </md-button>
+                    <md-button class="secondary " v-on:click="sendTestEmail">
+                        {{$t('Send me a test email')}}
+                    </md-button>
+                </md-card-actions>
+            </md-card>
         </div>
     </div>
 </template>
 
 <script>
 
-	import {get, set, sendTestmail} from 'js/Models/Setting';
+    import {get, set, sendTestmail} from 'js/Models/Setting';
 
 
-	export default {
+    export default {
 
-		data: () => {
-			return {
-				mail_smtp_host: '',
-				mail_smtp_port: '',
-				mail_smtp_user: '',
-				mail_smtp_password: '',
-				mail_smtp_frommail: '',
-				mail_smtp_fromname: '',
-			}
-		},
+        data: () => {
+            return {
+                mail_smtp_host: '',
+                mail_smtp_port: '',
+                mail_smtp_user: '',
+                mail_smtp_password: '',
+                mail_smtp_frommail: '',
+                mail_smtp_fromname: '',
+            }
+        },
 
-		mounted: function () {
-			get('mail_smtp_host').then((data) => {
-				this.mail_smtp_host = typeof data.val !== 'undefined' ? data.val : ''
-			});
-			get('mail_smtp_port').then((data) => {
-				this.mail_smtp_port = typeof data.val !== 'undefined' ? data.val : ''
-			});
-			get('mail_smtp_user').then((data) => {
-				this.mail_smtp_user = typeof data.val !== 'undefined' ? data.val : ''
-			});
-			get('mail_smtp_password').then((data) => {
-				this.mail_smtp_password = typeof data.val !== 'undefined' ? '**********' : ''
-			});
-			get('mail_smtp_frommail').then((data) => {
-				this.mail_smtp_frommail = typeof data.val !== 'undefined' ? data.val : ''
-			});
-			get('mail_smtp_fromname').then((data) => {
-				this.mail_smtp_fromname = typeof data.val !== 'undefined' ? data.val : ''
-			});
-		},
-		methods: {
-			saveEmail: async function () {
-				await set('mail_smtp_host', this.mail_smtp_host);
-				await set('mail_smtp_port', this.mail_smtp_port);
-				await set('mail_smtp_user', this.mail_smtp_user);
-				if (this.mail_smtp_password != '**********') {
-					await set('mail_smtp_password', this.mail_smtp_password);
-				}
-				await set('mail_smtp_frommail', this.mail_smtp_frommail);
-				await set('mail_smtp_fromname', this.mail_smtp_fromname);
-			},
-			sendTestEmail : async function() {
-				try {
-					let response = await sendTestmail();
-					if (response.status == 'ok') {
-						alert('A test email was sent to :' + response.sent_to);
-					}
-					else {
-						alert('Error occured : '+response.message);
-					}
+        mounted: function () {
+            get('mail_smtp_host').then((data) => {
+                this.mail_smtp_host = typeof data.val !== 'undefined' ? data.val : ''
+            });
+            get('mail_smtp_port').then((data) => {
+                this.mail_smtp_port = typeof data.val !== 'undefined' ? data.val : ''
+            });
+            get('mail_smtp_user').then((data) => {
+                this.mail_smtp_user = typeof data.val !== 'undefined' ? data.val : ''
+            });
+            get('mail_smtp_password').then((data) => {
+                this.mail_smtp_password = typeof data.val !== 'undefined' ? '**********' : ''
+            });
+            get('mail_smtp_frommail').then((data) => {
+                this.mail_smtp_frommail = typeof data.val !== 'undefined' ? data.val : ''
+            });
+            get('mail_smtp_fromname').then((data) => {
+                this.mail_smtp_fromname = typeof data.val !== 'undefined' ? data.val : ''
+            });
+        },
+        methods: {
+            saveEmail: async function () {
+                await set('mail_smtp_host', this.mail_smtp_host);
+                await set('mail_smtp_port', this.mail_smtp_port);
+                await set('mail_smtp_user', this.mail_smtp_user);
+                if (this.mail_smtp_password != '**********') {
+                    await set('mail_smtp_password', this.mail_smtp_password);
+                }
+                await set('mail_smtp_frommail', this.mail_smtp_frommail);
+                await set('mail_smtp_fromname', this.mail_smtp_fromname);
+            },
+            sendTestEmail: async function () {
+                try {
+                    let response = await sendTestmail();
+                    if (response.status == 'ok') {
+                        alert('A test email was sent to :' + response.sent_to);
+                    } else {
+                        alert('Error occured : ' + response.message);
+                    }
 
-				}
-				catch(error) {
-					alert('Error occured , please check log file or contract your administrator');
+                } catch (error) {
+                    alert('Error occured , please check log file or contract your administrator');
                 }
             }
-		}
-	}
+        }
+    }
 
 </script>
