59d4191ad3f2da38e52f2982f680a407b55655bb
Loïc <loic@webismymind.be>
2021-08-31 14:57:40 +0200

    Added a hook allowing modules to provide their translations.

--- public/src/js/Translations/_translations.js
+++ public/src/js/Translations/_translations.js
@@ -2,6 +2,8 @@
 import fr from 'js/Translations/fr';
 import en from 'js/Translations/en';
 
+import {getHooks} from 'js/Services/HookHandler';
+
 import Vue from 'vue'
 import VueI18n from 'vue-i18n';
 
@@ -10,12 +12,20 @@ const user = localStorage.getItem('user')  ?  JSON.parse(localStorage.getItem('u
 
 Vue.use(VueI18n);
 
+var langsHooks = getHooks('register_lang_strings');
+const messages = {};
+langsHooks.forEach((langItem) => {
+    langItem = langItem();
+    messages[langItem.langcode] = typeof messages[langItem.langcode] !== 'undefined' ? {...messages[langItem.langcode],  ...langItem.messages} :  langItem.messages;
+
+});
+
+messages.fr = typeof messages.fr !== 'undefined' ? {...messages.fr, ...fr} : fr;
+messages.en = typeof messages.en !== 'undefined' ? {...messages.en, ...en} : en;
+console.log(messages);
 
 export default new VueI18n({
     locale : typeof user.options.lang !== 'undefined' ? user.options.lang : Config.defaultLang || 'en',
     fallbackLocale: 'en',
-    messages : {
-        fr : fr,
-        en : en
-    }
+    messages : messages
 })
