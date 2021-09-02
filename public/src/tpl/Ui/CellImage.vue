d0ddf378dcb06522395a14a7c1c88c19a6f74282
Loïc <loic@webismymind.be>
2021-09-02 16:07:53 +0200

    CellImage

---
+++ public/src/tpl/Ui/CellImage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="cell cell-image">
+        <img :src="value" alt="" />
+    </div>
+</template>
+
+
+<script>
+	export default {
+		props: ['component', 'item', 'value'],
+	}
+</script>
