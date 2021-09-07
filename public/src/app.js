7aefe74d4260509e1c95d2f3c4d72478a47ff4c1
Loïc <loic@webismymind.be>
2021-09-07 11:04:24 +0200

    Added vue-select as dependency

--- public/src/app.js
+++ public/src/app.js
@@ -3,6 +3,7 @@ import 'vue-simple-markdown/dist/vue-simple-markdown.css';
 import 'vue-material-design-icons/styles.css';
 import 'vue-datetime/dist/vue-datetime.min.css';
 import 'vue-material/dist/vue-material.min.css';
+import 'vue-select/dist/vue-select.css';
 
 import 'vue-material/dist/theme/default.css';
 import 'css/app.scss'
@@ -11,6 +12,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import VueMaterial from 'vue-material'
+import vSelect from 'vue-select'
 
 
 import {Main} from 'js/App';
@@ -19,6 +21,7 @@ Vue.use(VueRouter);
 Vue.use(VueSimpleMarkdown);
 Vue.use(Vuex);
 Vue.use(VueMaterial);
+Vue.component('v-select', vSelect)
 
 new Main();
 
