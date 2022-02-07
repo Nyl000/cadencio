2528f4ca28c3b8ce1dd7710fe4edbab191014021
Loïc <loic@webismymind.be>
2022-02-07 10:47:40 +0100

    fix

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__.'../src/vendor/autoload.php';
-require_once __DIR__.'../config.php';
+require_once __DIR__.'/../src/vendor/autoload.php';
+require_once __DIR__.'/../config.php';
 
 $app = new Cadencio\Application();
 $app->run();
